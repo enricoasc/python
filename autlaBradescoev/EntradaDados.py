4bd79cdfba599f3dd868bd2be9e30079229cfca7
Enrico Augusto <informatica@riobrancopetroleo.com.br>
2024-01-17 12:01:33 -0300

    Python Básico Bradesco EV

---
+++ autlaBradescoev/EntradaDados.py
@@ -0,0 +1,28 @@
+# fruta = input('Digite o nome da fruta :')
+# print(fruta)
+
+# --------------------------------
+
+codigo = int(input('Digiete o código do funcionário :'))
+nome = input('Digite o Nome do funcionário :')
+salario = float(input('Infome o salário :'))
+ativo = True
+
+print('\nCódigo : %d' %codigo )
+print('Nome: %s' %nome)
+print('Salario : %.2f' %salario)
+print('Status : %r' %ativo)
+
+# Sequência	    Descrição
+# \n	        Insere uma quebra de linha.
+# \t			Insere tabulação horizontal.
+# \v	        Insere tabulação vertical.
+# \r			Equivalente ao efeito da tecla Enter.
+# \’			Aspas simples.
+# \”			Aspas duplas.
+# \\			Insere uma barra invertida (backslash).
+# \a			Chamado de ASC bell ou beep do sistema. Se houver suporte, aciona um bipe.
+# \b			Aciona o backspace, ou seja, apaga o caractere anterior.
+# \f			Insere uma quebra de página.
+# \u			Insere um caractere UNICODE. Deve acompanhar um código com 4 números.
+
--- autlaBradescoev/TipoVariavel.py
+++ autlaBradescoev/TipoVariavel.py
@@ -7,3 +7,25 @@ tipo = type(salario)
 
 print(salario)
 print(tipo)
+
+print('Codigo: ',codigo,' Nome: ',nome,' O salario atual é de :',salario)
+print('Código: '+str(codigo)+' Nome: '+nome+' O salário atual é de : '+str(salario))
+
+# ------------------------------------------------------------------------------
+
+# Máscara	        Tipo de             Descrição
+# %d ou %i	        Int (inteiro)	    Exibe um valor inteiro.
+# %f	            Float ou double	    Exibe um valor decimal.
+# %ld	            Long Int	        Exibe um número inteiro longo.
+# %e ou %E	        Float e double	    Exibe um número exponencial (número científico).
+# %c	            Char (caractere)	Exibe um caractere.
+# %o	            Int	                Exibe um número inteiro em formato octal.
+# %x ou %X	        Int	                Exibe um número inteiro no formato hexadecimal.
+# %s	            Char	            Exibe uma cadeia de caracteres (string).
+# %r	            Boolean	            Exibe true ou false (verdadeiro ou falso).
+
+# ------------------------------------------------------------------------------
+print('Código : %d' %codigo )
+print('Nome: %s' %nome)
+print('Salario : %.2f' %salario)
+print('Status : %r' %situacao)
